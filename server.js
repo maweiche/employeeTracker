2d8264429b061b7588ab2407115ad9ff4997e7da
Matt Weichel <maweiche@gmail.com>
2021-10-24 16:23:53 -0700

    fixed database connection issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 const fs = require('fs');
-const Connection = require('mysql2/typings/mysql/lib/Connection');
+const inquirer = require('inquirer');
 //port
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: 'employee_db'
+        database: 'employees_db'
     },
     console.log('Connected to employee_db database')
 );
@@ -36,4 +36,36 @@ startImage = () => {
     console.log("*                  *")
     console.log("********************")
     promptUser();
+}
+
+//inquirer prompts
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices:   ['View all departments',
+                        'View all roles',
+                        'View all employees',
+                        'Add a department',
+                        'Add a role',
+                        'Add an employee',
+                        'Update an employee role',
+                        'Update an employee manager',
+                        'View employees by department',
+                        'Delete a department',
+                        'Delete a role',
+                        'Delete an employee',
+                        'No Action']
+        }
+    ])
+    .then((answers) => {
+        const { choices } = answers;
+
+        if (choices === "View all departments") {
+            showDepartments();
+        }
+
+    })
 }
